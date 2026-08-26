4421ca6a47c102da3230fe45cc1af611418fb18a
basha7865 <167752448+basha7865@users.noreply.github.com>
2026-08-26 11:43:39 +0530

    Update settings.py

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%5f9eqi_d6yrb6uy&gwd1%fi46d#!4foqrp4m80zrnf^_!g0=c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
